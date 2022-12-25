a42bc5c41aea65873e64e9117cc917cfea13e152
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2022-12-25 19:54:40 +0500

    chore: initial routing setup

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,18 +2,20 @@ import Home from "../pages/Home"
 
 // BASIC URLS
 export const URL_HOME = "/"
+export const URL_LOGIN = "/login"
+export const URL_SIGNUP = "/signup"
 
 export const OPEN_ROUTES = [
   {
-    path: "/",
+    path: URL_HOME,
     element: <Home />,
   },
   {
-    path: "/signup",
+    path: URL_SIGNUP,
     element: <h1>Sign Up page</h1>
   },
   {
-    path: "/login",
+    path: URL_LOGIN,
     element: <h1>Login Page</h1>
   }
 ]
