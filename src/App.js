848e7ab22eca20b5e63521e49e971334abf73345
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2022-12-30 16:20:25 +0500

    started landing page development (#1)
    * chore: started landing page development
    
    * add: custom theme for light and dark mode
    
    * landing page setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { CssBaseline } from "@mui/material"
 import { RouterProvider } from "react-router-dom"
 import { Router } from "./router/router"
+import {ThemeProvider} from '@mui/material'
+import { LIGHTTHEME, DARKTHEME } from './styles/MuiTheme'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={LIGHTTHEME}>
       <CssBaseline />
       <RouterProvider router={Router} />
-    </>
+    </ThemeProvider>
   )
 }
 
